63eccfe081c5901b8d5626a1d7dd9ae189568ff1
hauct <cungtronghau@gmail.com>
2024-07-18 22:46:04 +0700

    'edit'

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -6,7 +6,9 @@ WITH dim_supplier__source AS (
 , dim_supplier__rename_column AS (
   SELECT 
     supplier_id  AS supplier_key,
-    supplier_name
+    supplier_name,
+    supplier_category_id AS supplier_category_key,
+    delivery_method_id AS delivery_method_key
   FROM dim_supplier__source
 )
 
@@ -14,25 +16,42 @@ WITH dim_supplier__source AS (
   SELECT 
     CAST(supplier_key AS INTEGER)  AS supplier_key,
     CAST(supplier_name AS STRING) AS supplier_name,
+    CAST(supplier_category_key AS INTEGER)  AS supplier_category_key,
+    CAST(delivery_method_key AS INTEGER)  AS delivery_method_key,
   FROM dim_supplier__rename_column
  )
 
 , dim_supplier__add_undefined_record AS (
   SELECT 
     supplier_key,
-    supplier_name
+    supplier_name,
+    supplier_category_key,
+    delivery_method_key
   FROM dim_supplier__cast_type
   UNION ALL
   SELECT
     0 AS supplier_key,
-    'Undefined' AS supplier_name
+    'Undefined' AS supplier_name,
+    0 AS supplier_category_key,
+    0 AS delivery_method_key
   UNION ALL
   SELECT
     -1 AS supplier_key,
-    'Invalid' AS supplier_name
+    'Invalid' AS supplier_name,
+    -1 AS supplier_category_key,
+    -1 AS delivery_method_key
 )
 
 SELECT
-  supplier_key,
-  supplier_name
-FROM dim_supplier__add_undefined_record
+  dim_supplier.supplier_key,
+  dim_supplier.supplier_name,
+  dim_supplier.supplier_category_key,
+  COALESCE(dim_supplier_category.supplier_category_name, 'Invalid') AS supplier_category_name,
+  dim_supplier.delivery_method_key,
+  COALESCE(dim_delivery_method.delivery_method_name, 'Invalid') AS delivery_method_name
+FROM dim_supplier__add_undefined_record AS dim_supplier
+LEFT JOIN {{ref('stg_dim_supplier_category')}} AS dim_supplier_category
+ON dim_supplier.supplier_category_key = dim_supplier_category.supplier_category_key
+LEFT JOIN {{ref('stg_dim_delivery_method')}} AS dim_delivery_method
+ON dim_supplier.delivery_method_key = dim_delivery_method.delivery_method_key
+
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -11,7 +11,7 @@ WITH fact_sales_order_lines__source AS (
     quantity,
     unit_price,
     tax_rate,
-    picking_completed_when
+    picking_completed_when AS line_picking_completed_when
   FROM fact_sales_order_lines__source
 )
 
@@ -23,7 +23,7 @@ WITH fact_sales_order_lines__source AS (
     CAST(quantity AS INTEGER) AS quantity,
     CAST(unit_price AS NUMERIC) AS unit_price,
     CAST(tax_rate AS NUMERIC) AS tax_rate,
-    CAST(picking_completed_when AS DATE) AS picking_completed_when
+    CAST(line_picking_completed_when AS DATE) AS line_picking_completed_when
   FROM fact_sales_order_lines__rename_column
 )
 
@@ -34,9 +34,10 @@ WITH fact_sales_order_lines__source AS (
     product_key,
     quantity,
     unit_price,
+    tax_rate,
     quantity * unit_price AS gross_amount,
-    tax_rate * quantity * unit_price AS tax_amount
-    picking_completed_when,
+    tax_rate * quantity * unit_price AS tax_amount,
+    line_picking_completed_when
   FROM fact_sales__order_lines__cast_type
 )
 
@@ -46,11 +47,16 @@ SELECT
   fact_line.product_key,
   COALESCE(fact_header.customer_key, -1) AS customer_key,
   COALESCE(fact_header.picked_by_person_key, -1) AS picked_by_person_key,
-  fact_header.order_date,
+  COALESCE(fact_header.is_undersupply_backordered, 'Invalid') AS is_undersupply_backordered,
   fact_line.quantity, 
   fact_line.unit_price,
-  fact_line.gross_amount
+  fact_line.tax_rate,
+  fact_line.gross_amount,
+  fact_line.tax_amount,
+  COALESCE(fact_header.expected_delivery_date, CAST('1900-01-01' AS DATE)) AS expected_delivery_date,
+  COALESCE(fact_header.order_date, CAST('1900-01-01' AS DATE)) AS order_date,
+  fact_line.line_picking_completed_when,
+  COALESCE(fact_header.picking_completed_when, CAST('1900-01-01' AS DATE)) AS order_picking_completed_when
 FROM fact_sales_order_lines__calculate_measure AS fact_line
 LEFT JOIN {{ref('stg_fact_sales_order')}} AS fact_header
 ON fact_line.sales_order_key = fact_header.sales_order_key
-
